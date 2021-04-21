dec10727db9d32602bf283b40a0e166495e1f149
Dion Johnson <dionjwa@gmail.com>
2021-04-20 18:22:58 -0700

    update server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const express = require('express');
-require('data/weather.json).config();
+const activate = require('./data/weather.json');
+console.log(activate);
 const cors = require('cors');
 
 const app = express();
@@ -11,3 +12,26 @@ app.use(cors());
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`listneing on ${PORT}`));
+
+app.get('/weather', (request, response) => {
+// const lat = request.query.lat;
+// const lon = request.query.lon;
+
+  const newArray = [];
+  for (let i = 0; i < activate.data.length; i++) {
+    const allData = new Data(activate.data[i].datetime, activate.data[i].weather.description)
+
+
+    newArray.push(allData)
+
+  }
+
+  // app.get('/weather', (request, response) => {
+  response.status(200).json(newArray);
+});
+
+function Data(date, description) {
+  this.date = date;
+  this.description = description;
+
+};
